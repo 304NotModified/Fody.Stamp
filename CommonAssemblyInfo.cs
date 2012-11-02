759e9ddb53271dfa9335a3b27e452749a9b22280
Simon Cropp <simon.cropp@gmail.com>
2012-11-03 10:25:04 +1100

    use ILMerge

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -2,5 +2,5 @@
 
 [assembly: AssemblyTitle("Stamp")]
 [assembly: AssemblyProduct("Stamp")]
-[assembly: AssemblyVersion("0.1.0.0")]
-[assembly: AssemblyFileVersion("0.1.0.0")]
+[assembly: AssemblyVersion("0.1.1.0")]
+[assembly: AssemblyFileVersion("0.1.1.0")]
