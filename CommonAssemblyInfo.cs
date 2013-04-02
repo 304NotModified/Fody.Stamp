b93c3df5c5655cf655cb09f480dbed450be602eb
Simon Cropp <simon.cropp@gmail.com>
2013-04-02 22:11:11 +1100

    fix nuget cleaning

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -2,5 +2,5 @@
 
 [assembly: AssemblyTitle("Stamp")]
 [assembly: AssemblyProduct("Stamp")]
-[assembly: AssemblyVersion("0.5.1.1")]
-[assembly: AssemblyFileVersion("0.5.1.1")]
+[assembly: AssemblyVersion("0.5.1.2")]
+[assembly: AssemblyFileVersion("0.5.1.2")]
