f23cef4f6603467cd05e6333940381737d4e9e53
Simon Cropp <simon.cropp@gmail.com>
2014-05-11 19:26:09 +1000

    bump version

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -2,5 +2,5 @@
 
 [assembly: AssemblyTitle("Stamp")]
 [assembly: AssemblyProduct("Stamp")]
-[assembly: AssemblyVersion("0.8.2")]
-[assembly: AssemblyFileVersion("0.8.2")]
+[assembly: AssemblyVersion("0.8.3")]
+[assembly: AssemblyFileVersion("0.8.3")]
