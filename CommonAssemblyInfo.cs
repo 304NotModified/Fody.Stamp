82070f3c4db4c26933564b89531a6ceaa0972dbe
Jens Vegeby <jens@vegeby.nu>
2014-11-29 20:22:13 +0100

    Bumped version to 1.2.0

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -2,5 +2,5 @@
 
 [assembly: AssemblyTitle("Stamp")]
 [assembly: AssemblyProduct("Stamp")]
-[assembly: AssemblyVersion("1.1.0")]
-[assembly: AssemblyFileVersion("1.1.0")]
+[assembly: AssemblyVersion("1.2.0")]
+[assembly: AssemblyFileVersion("1.2.0")]
