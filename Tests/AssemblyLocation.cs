450c6a4e07fbd7ccea9553d748742c4791ca6e46
Julian Verdurmen <304NotModified@users.noreply.github.com>
2018-09-01 00:52:23 +0200

    Fix net45

--- Tests/AssemblyLocation.cs
+++ Tests/AssemblyLocation.cs
@@ -4,19 +4,27 @@ using NUnit.Framework;
 
 public static class AssemblyLocation
 {
-	public static string CurrentDirectory()
-	{
-		var assembly = typeof(AssemblyLocation).Assembly;
-		var uri = new UriBuilder(assembly.CodeBase);
-		var path = Uri.UnescapeDataString(uri.Path);
+    public static string CurrentDirectory()
+    {
+        var assembly = typeof(AssemblyLocation).Assembly;
+        var uri = new UriBuilder(assembly.CodeBase);
+        var path = Uri.UnescapeDataString(uri.Path);
 
-		return Path.GetDirectoryName(path);
-	}
+        return Path.GetDirectoryName(path);
+    }
 
     public static string CreateBeforeAssemblyPath()
     {
         var beforeAssemblyPath = Path.GetFullPath(Path.Combine(TestContext.CurrentContext.TestDirectory, @"..\..\..\..\AssemblyToProcessExistingAttribute\bin\Debug\"));
+
+#if NETCORE21
         beforeAssemblyPath = Path.Combine(beforeAssemblyPath, @"netcoreapp2.0");
+#elif NET452
+         beforeAssemblyPath = Path.Combine(beforeAssemblyPath, @"net452");
+#else
+#error Fix path for this platform
+
+#endif
         beforeAssemblyPath = Path.Combine(beforeAssemblyPath, @"AssemblyToProcessExistingAttribute.dll");
 #if (!DEBUG)
         beforeAssemblyPath = beforeAssemblyPath.Replace("Debug", "Release");
